2f1e4c900dcb0250c4df3ad2a63e1491ba9d26a1
Yusuke Inoue <yi@Yusuke-no-MacBook-Air.local>
2019-07-16 20:05:17 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+  const [blogData, setBlogData] = useState(false);
+
+  const fetchData = () => {
+    console.log('fetch');
+    let fetchDataArray = [];
+    fetchDataArray.push(
+      <div key={1}>test</div>
+    )
+    setBlogData(fetchDataArray);
+  }
+  return(
+    <div>
+      <button onClick={ () => { fetchData(); } }>フェッチデータ</button>
+      { blogData ? <div>{ blogData }</div> : <div>Nothing</div> }
     </div>
   );
 }
